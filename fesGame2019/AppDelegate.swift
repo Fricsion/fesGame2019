8f55e474b844c338f68a06c6718ba9af1a2842f6
Tiz <taikikku3291@icloud.com>
2019-08-31 14:09:15 +0900

    Initial Commit

---
+++ fesGame2019/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  fesGame2019
+//
+//  Created by Tiz Matz on 2019/08/31.
+//  Copyright © 2019 Tiz'sMake. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
